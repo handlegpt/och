ff201238df62d7b930379433c8866a08c0175a53
tomi <tomi@tomi-2.local>
2025-09-19 10:11:17 +0900

    chore: add React Router v7 future flags to suppress warnings
    - Add v7_startTransition and v7_relativeSplatPath future flags
    - Suppress React Router v6 deprecation warnings
    - Prepare for React Router v7 migration

--- index.tsx
+++ index.tsx
@@ -1,27 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { LanguageProvider } from './i18n/context';
-import { ThemeProvider } from './theme/context';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { AuthProvider } from './src/components/auth/AuthProvider';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import { LanguageProvider } from './i18n/context'
+import { ThemeProvider } from './theme/context'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { AuthProvider } from './src/components/auth/AuthProvider'
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById('root')
 if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+  throw new Error('Could not find root element to mount to')
 }
 
-const root = ReactDOM.createRoot(rootElement);
+const root = ReactDOM.createRoot(rootElement)
 root.render(
   <React.StrictMode>
     <LanguageProvider>
       <ThemeProvider>
         <AuthProvider>
-          <Router>
+          <Router future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
             <App />
           </Router>
         </AuthProvider>
       </ThemeProvider>
     </LanguageProvider>
   </React.StrictMode>
-);
+)
